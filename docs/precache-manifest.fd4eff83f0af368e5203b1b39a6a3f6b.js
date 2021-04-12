c4ec2c04e91bb215db80cce57bcdf87e03e99ff4
Poes1a <manuelhenriquezgacitua@gmail.com>
2021-04-12 19:52:31 -0400

    V1.1

---
+++ docs/precache-manifest.fd4eff83f0af368e5203b1b39a6a3f6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c00a2e4c31b8d06243cdf83b827495",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74429e88d36de488aab9",
+    "url": "/static/css/main.98a4c021.chunk.css"
+  },
+  {
+    "revision": "21128ddbf1ff63946978",
+    "url": "/static/js/2.31a10122.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.31a10122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74429e88d36de488aab9",
+    "url": "/static/js/main.cf46a9f5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
