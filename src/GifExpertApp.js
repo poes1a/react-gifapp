18d589d2df9487b100349cda2e0601352a2d79e3
Poes1a <manuelhenriquezgacitua@gmail.com>
2021-04-12 22:19:34 -0400

    V1.3

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -5,7 +5,7 @@ import { GifGrid } from './componenetes/GifGrid';
 export const GifExpertApp = () => {
 
     //const categorias = ['One Punch', 'Samurai X', 'Dragon Ball'];
-    const [categorias, setCategorias] = useState(['DragonBall']);
+    const [categorias, setCategorias] = useState(['']);
     /*const handleAdd = () =>{
        // setCategorias([...categorias,'Vegeta']);
         setCategorias(cats =>[...cats,'Vegeta ssj']);
@@ -13,7 +13,7 @@ export const GifExpertApp = () => {
     return (
         <>
 
-           <h2>GifExpertApp</h2>
+           <h2>Buscador de Gifs</h2>
            <AddCategory setCategorias={setCategorias}/>
            <hr />
            <ol>
